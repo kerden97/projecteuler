60d488500f947e608bac8201e80350520557c41b
Kerem Erden <63859357+kerden97@users.noreply.github.com>
2020-11-16 13:08:44 +0000

    Project Euler 4th Problem Solution w Python

---
+++ PythonSolutions/ProblemFour.py
@@ -0,0 +1,34 @@
+##
+#  the largest palindrome made from the product of two 3-digit numbers.
+#  Fourth problem of ProjectEuler.net
+#
+#  @author Kerem ERDEN
+
+
+# A palindromic number reads the same both ways. The largest palindrome
+# made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+#  Return the largest palindrome made from the product of two 3-digit numbers.
+#
+#  @return largestPalindromeNumber
+def largestPalindrome():
+    largestPalindromeNumber = 0
+    for firstNumber in range(100, 1000):  # 1000 is exclusive , 3-digit numbers only
+        for secondNumber in range(100, 1000):
+            numbersProduct = firstNumber * secondNumber
+            if palindromeCheck(numbersProduct) and numbersProduct > largestPalindromeNumber:
+                largestPalindromeNumber = numbersProduct
+
+    return largestPalindromeNumber
+
+
+#  Check if a number palindrome number or not according to definition in the question
+#  @param number // product of two 3-digit numbers
+#
+#  @return boolean True | False
+def palindromeCheck(number):
+    strNumber = str(number)
+    if strNumber == strNumber[::-1]:  # python allow us to reverse a string easily like this
+        return True
+    return False
+
